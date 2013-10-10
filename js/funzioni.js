0ed3160bd77e8754adf6fadd75895742cf2e21e3
toplus <toplus90@alice.it>
2013-10-10 14:19:45 +0200

    Extended app

--- js/funzioni.js
+++ js/funzioni.js
@@ -1,3 +1,18 @@
-function traduci( nome ){
-	document.getElementById("nome2").innerHTML = "Rutto di culo";
+function traduci(nome){
+	if(nome == "mattia")
+		document.getElementById('nome2').innerHTML = 'Rutto di culo';
+	else{
+		
+		switch(Math.ceil(Math.random()*4)){
+			case 1: nome2 = 'Rombo di Tuono';
+				break;
+			case 2: nome2 = 'Toro seduto';
+				break;
+			case 3: nome2 = 'Nuvola rossa';
+				break;
+			case 4: nome2 = 'Occhio di volpe';
+				break;
+		}
+		document.getElementById('nome2').innerHTML = nome2;	
+	}
 }
